660bc344cfc11d1c4de40c420bff982aea6205d3
AndyButland <abutland73@gmail.com>
2016-07-29 22:54:52 +0200

    Restricted the default order for a list view to be selected from the list of included columns from the list view. Allowed usage of system or custom fields for the default order of the list view.

--- src/Umbraco.Web.UI.Client/src/views/propertyeditors/listview/sortby.prevalues.controller.js
+++ src/Umbraco.Web.UI.Client/src/views/propertyeditors/listview/sortby.prevalues.controller.js
@@ -1,6 +1,31 @@
-function sortByPreValsController($rootScope, $scope, localizationService) {
+function sortByPreValsController($rootScope, $scope, localizationService, editorState) {
 
-    $scope.sortByFields = [
+    // Helper to find a particular value from the list of sort by options
+    function findFromSortByFields(value) {
+        return _.find($scope.sortByFields, function (e) {
+            return e.value.toLowerCase() === value.toLowerCase();
+        });
+    }
+
+    // Get list of properties assigned as columns of the list view
+    var propsPreValue = _.findWhere(editorState.current.preValues, { key: "includeProperties" });
+
+    // Populate list of options for the default sort (all the columns plus then node name)
+    $scope.sortByFields = [];
+    $scope.sortByFields.push({ value: "name", name: "Name", isSystem: 1 });
+    if (propsPreValue != undefined) {
+        for (var i = 0; i < propsPreValue.value.length; i++) {
+            var value = propsPreValue.value[i];
+            $scope.sortByFields.push({
+                value: value.alias,
+                name: value.header,
+                isSystem: value.isSystem
+            });
+        }
+    }
+
+    // Localize the system fields, for some reason the directive doesn't work inside of the select group with an ng-model declared
+    var systemFields = [
         { value: "SortOrder", key: "general_sort" },
         { value: "Name", key: "general_name" },
         { value: "VersionDate", key: "content_updateDate" },
@@ -12,30 +37,41 @@ function sortByPreValsController($rootScope, $scope, localizationService) {
         { value: "Email", key: "general_email" },
         { value: "Username", key: "general_username" }
     ];
-    
-    //now we'll localize these strings, for some reason the directive doesn't work inside of the select group with an ng-model declared
-    _.each($scope.sortByFields, function (e, i) {
+    _.each(systemFields, function (e) {
         localizationService.localize(e.key).then(function (v) {
-            e.name = v;
 
-            switch (e.value) {
-                case "Updater":
-                    e.name += " (Content only)";
-                    break;
-                case "Published":
-                    e.name += " (Content only)";
-                    break;
-                case "Email":
-                    e.name += " (Members only)";
-                    break;
-                case "Username":
-                    e.name += " (Members only)";
-                    break;
+            var sortByListValue = findFromSortByFields(e.value);
+            if (sortByListValue) {
+                sortByListValue.name = v;
+                switch (e.value) {
+                    case "Updater":
+                        e.name += " (Content only)";
+                        break;
+                    case "Published":
+                        e.name += " (Content only)";
+                        break;
+                    case "Email":
+                        e.name += " (Members only)";
+                        break;
+                    case "Username":
+                        e.name += " (Members only)";
+                        break;
+                }
             }
         });
     });
 
+    // Check existing model value is available in list and ensure a value is set
+    var existingValue = findFromSortByFields($scope.model.value);
+    if (existingValue) {
+        // Set the existing value
+        // The old implementation pre Umbraco 7.5 used PascalCase aliases, this uses camelCase, so this ensures that any previous value is set
+        $scope.model.value = existingValue.value;
+    }
+    else {
+        // Existing value not found, set to first value
+        $scope.model.value = $scope.sortByFields[0].value;
+    }
 }
-
 
 angular.module("umbraco").controller("Umbraco.PrevalueEditors.SortByListViewController", sortByPreValsController);
